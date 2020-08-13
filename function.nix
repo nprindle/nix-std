40afccc54e803601dcab0814b7c7bc4264c89620
nprindle <nprindle18@gmail.com>
2020-08-13 03:22:32 -0400

    Add more functions to function.nix

--- function.nix
+++ function.nix
@@ -7,6 +7,10 @@ rec {
   */
   const = a: _: a;
 
+  /* apply :: (a -> b) -> a -> b
+  */
+  apply = f: x: f x;
+
   /* compose :: (b -> c) -> (a -> b) -> (a -> c)
   */
   compose = bc: ab: a: bc (ab a);
@@ -14,4 +18,12 @@ rec {
   /* flip :: (a -> b -> c) -> b -> a -> c
   */
   flip = f: b: a: f a b;
+
+  /* on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+  */
+  on = f: proj: x: y: f (proj x) (proj y);
+
+  /* pipe :: a -> [a -> a] -> a
+  */
+  pipe = x0: fs: builtins.foldl' (flip apply) fs x0;
 }
