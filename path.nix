3aff56a9f06db9daf1e316c8a04c7e243ae16972
Nicole Prindle <nprindle18@gmail.com>
2021-06-22 23:33:54 -0400

    Add path.normalize and path.components

--- path.nix
+++ path.nix
@@ -1,6 +1,57 @@
 let
   set = import ./set.nix;
   list = import ./list.nix;
+  string = import ./string.nix;
+  regex = import ./regex.nix;
+  fixpoints = import ./fixpoints.nix;
+  function = import ./function.nix;
 in rec {
   inherit (builtins) baseNameOf dirOf;
+
+  isAbsolute = path: string.hasPrefix "/" path;
+
+  /* normalize :: string | path -> string
+  */
+  normalize = path:
+    fixpoints.fixEq
+      (function.chain [
+        # remove duplicate separators
+        (regex.substitute "/{2,}" "/")
+        # remove intermediate "/./"
+        (regex.substitute ''(.)/\./'' ''\1/'')
+        # remove leading "/./" (except if path is only "/.")
+        (x:
+          if x == "/."
+          then "/"
+          else regex.substitute ''^/\./'' "/" x
+        )
+        # remove trailing "/." (except if path is only "/.")
+        (x:
+          if x == "/."
+          then "/"
+          else regex.substitute ''/\.$'' "" x
+        )
+        # remove leading "./"
+        (x:
+          if x == "./"
+          then "."
+          else string.removePrefix "./" x
+        )
+        # remove trailing "/" (except if path is only "/")
+        (x:
+          if x == "/"
+          then "/"
+          else string.removeSuffix "/" x
+        )
+      ])
+      (builtins.toString path);
+
+  /* components :: string | path -> [string]
+  */
+  components = path:
+    let
+      path' = normalize path;
+      cs = regex.splitOn "/" path';
+      cs' = if isAbsolute path' then list.setAt 0 "/" cs else cs;
+    in cs';
 }
