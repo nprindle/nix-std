560ed547a088ff981a4589875297d4ab105d1bed
Nicole Prindle <nprindle18@gmail.com>
2021-06-25 14:08:22 -0400

    Add more bool functions

--- bool.nix
+++ bool.nix
@@ -14,4 +14,16 @@ rec {
   /* ifThenElse :: bool -> a -> a -> a
   */
   ifThenElse = b: x: y: if b then x else y;
+
+  /* xor :: bool -> bool -> bool
+  */
+  xor = x: y: x != y;
+
+  /* xnor :: bool -> bool -> bool
+  */
+  xnor = x: y: x == y;
+
+  /* implies :: bool -> bool -> bool
+  */
+  implies = x: y: x -> y;
 }
