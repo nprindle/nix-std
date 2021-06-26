e60cb6f9ac6d7ec715630fef2084b199aa2cefd4
Nicole Prindle <nprindle18@gmail.com>
2021-06-26 04:23:46 -0400

    Add path.parents

--- path.nix
+++ path.nix
@@ -3,17 +3,17 @@ let
   list = import ./list.nix;
   string = import ./string.nix;
   optional = import ./optional.nix;
+
+  joinComponents = parts:
+    if list.length parts > 0 && list.index parts 0 == "/"
+      then "/" + string.concatSep "/" (list.tail parts)
+      else string.concatSep "/" parts;
 in rec {
   inherit (builtins) baseNameOf dirOf;
 
   /* normalize :: string | path -> string
   */
-  normalize = path:
-    let
-      parts = components path;
-    in if list.length parts > 0 && list.index parts 0 == "/"
-      then "/" + string.concatSep "/" (list.tail parts)
-      else string.concatSep "/" parts;
+  normalize = path: joinComponents (components path);
 
   /* components :: string | path -> [string]
   */
@@ -35,5 +35,36 @@ in rec {
             }
           )
           (optional.just path);
-    in list.filter (p: p != "." && p != "") (list.reverse parts);
+    in if parts == [ "." ]
+      then parts
+      else list.filter (p: p != "." && p != "") (list.reverse parts);
+
+  /* parents :: path -> [path]
+
+     Get the successive parents of a path.
+
+     Can also handle path-like strings, but if the path is relative, the parents
+     will only continue until the base of the relative path (usually the current
+     directory).
+  */
+  parents = path:
+    if builtins.isPath path
+    then
+      let
+        parts = list.unfold
+          (p:
+            if p == /.
+            then optional.nothing
+            else optional.just { _0 = p; _1 = builtins.dirOf p; }
+          )
+          path;
+      in parts ++ [ /. ]
+    else
+      let
+        cs = components path;
+        go = acc: xs:
+          if list.length xs == 0
+          then acc
+          else go (acc ++ [(joinComponents xs)]) (list.init xs);
+      in go [] cs;
 }
